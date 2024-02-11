0ab1347d493605a7a890a77fda13e6dd19b8a1a9
contangosan <kai.mueller98@gmx.de>
2024-02-11 22:45:41 +0100

    ganzer dateipfad in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/intuitive_sculpting_2.0/"
+  base: "/Users/Kai/Documents/GitHub/intuitive_sculpting_2.0"
 })
